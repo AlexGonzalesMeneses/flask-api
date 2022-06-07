1a6c2deccbfaa9ca7bb34a5c329fac7bf1ceedf3
Alex Gonzales <Alex.Gonzales@fundacion-jala.org>
2022-06-07 17:55:38 -0400

    flask api

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from repository import Repository
 from item import Item
 
 app = Flask(__name__)
+CORS(app)
 items = Repository()
 
 
--- repository.py
+++ repository.py
@@ -6,11 +6,11 @@ import json
 class Repository:
     def __init__(self):
         self.items = [
-            Item("1", "Coca Cola", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/15-09-26-RalfR-WLC-0098.jpg/320px-15-09-26-RalfR-WLC-0098.jpg",
+            Item("1", "Coca Cola", "https://images.unsplash.com/photo-1567103472667-6898f3a79cf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29jYWNvbGElMjB8ZW58MHx8MHx8&w=1000&q=80",
                  "Mexico", "The Coke Company is a good drink", "1.00", "Litro", "Coca Cola"),
-            Item("2", "Fanta", "https://www.cocacola.com.mx/content/dam/coke/images/coca-cola/coca-cola-logo-1.png",
+            Item("2", "Fanta", "https://cdn.shopify.com/s/files/1/0368/1363/5716/products/7771609001165_709x709.jpg?v=1637172460",
                  "Mexico", "The Coke Company is a good drink", "1.00", "Litro", "Coca Cola"),
-            Item("3", "Sprite", "https://www.cocacola.com.mx/content/dam/coke/images/coca-cola/coca-cola-logo-1.png",
+            Item("3", "Sprite", "https://www.cocacola.es/content/dam/one/es/es2/sprite/home-page/sp-1-5-sp.jpg",
                  "Mexico", "The Coke Company is a good drink", "1.00", "Litro", "Coca Cola"),
         ]
 
